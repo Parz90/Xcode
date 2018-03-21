c8679f656fcd20cef501572caaa71dc99c4da994
Parthipan Nagulanandan <Parz@s030020a.adr.admin.ch>
2018-03-21 17:12:06 +0100

    test

---
+++ DockerManager/DockerManager/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DockerManager
+//
+//  Created by Parthipan Nagulanandan on 21.03.18.
+//  Copyright © 2018 Parthipan Nagulanandan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
